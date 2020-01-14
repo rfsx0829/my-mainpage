0c0ca13e25939d330e9cb7e63ec48f276eb93b58
rfsx0829 <rfsx0829@163.com>
2020-01-14 20:36:12 +0800

    fix bug

--- src/Home/Footer.jsx
+++ src/Home/Footer.jsx
@@ -16,7 +16,7 @@ function Footer() {
                 </a>
               </div>
               <div>
-                <a target="_blank" rel="noopener noreferrer" href="https://github.com/rfsx0829/">
+                <a target="_blank" rel="noopener noreferrer" href="https://github.com/rfsx0829/my-mainpage">
                   <FormattedMessage id="app.footer.sourcecode" />
                 </a>
               </div>
